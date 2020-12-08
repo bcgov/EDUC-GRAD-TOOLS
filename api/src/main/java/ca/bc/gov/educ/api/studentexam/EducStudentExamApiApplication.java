41357ff12bc2a30380c0c5bf49bb316dde8d6419
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2020-12-08 11:04:04 -0800

    KeyCloak Changes

--- api/src/main/java/ca/bc/gov/educ/api/studentexam/EducStudentExamApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentexam/EducStudentExamApiApplication.java
@@ -3,26 +3,23 @@ package ca.bc.gov.educ.api.studentexam;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.client.RestTemplate;
 
 import ca.bc.gov.educ.api.studentexam.model.dto.StudentExam;
 import ca.bc.gov.educ.api.studentexam.model.entity.StudentExamEntity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableCaching
 public class EducStudentExamApiApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(EducStudentExamApiApplication.class);
-
-	@Value("${spring.security.user.name}")
-	private String uName;
-	    
-	@Value("${spring.security.user.password}")
-	private String pass;
 	
 	public static void main(String[] args) {
 		logger.debug("########Starting API");
@@ -42,6 +39,6 @@ public class EducStudentExamApiApplication {
 	
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.basicAuthentication(uName, pass).build();
+		return builder.build();
 	}
 }
--- api/src/main/java/ca/bc/gov/educ/api/studentexam/controller/StudentExamController.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentexam/controller/StudentExamController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 import ca.bc.gov.educ.api.studentexam.model.dto.StudentExam;
 import ca.bc.gov.educ.api.studentexam.service.StudentExamService;
 import ca.bc.gov.educ.api.studentexam.util.StudentExamApiConstants;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @CrossOrigin
 @RestController
 @RequestMapping(StudentExamApiConstants.STUDENT_EXAM_API_ROOT_MAPPING)
+@EnableResourceServer
+@OpenAPIDefinition(info = @Info(title = "API for Student Exam Data.", description = "This Read API is for Reading Student Exam data.", version = "1"), security = {@SecurityRequirement(name = "OAUTH2", scopes = {"READ_GRAD_STUDENT_EXAM_DATA"})})
 public class StudentExamController {
 
     private static Logger logger = LoggerFactory.getLogger(StudentExamController.class);
@@ -26,8 +35,11 @@ public class StudentExamController {
     StudentExamService studentAssessmentService;
 
     @GetMapping(StudentExamApiConstants.GET_STUDENT_EXAM_BY_PEN_MAPPING)
+    @PreAuthorize("#oauth2.hasScope('READ_GRAD_STUDENT_EXAM_DATA')")
     public List<StudentExam> getStudentExamByPEN(@PathVariable String pen) {
         logger.debug("#Get All Student Exams by PEN: " + pen);
-        return studentAssessmentService.getStudentExamList(pen);
+        OAuth2AuthenticationDetails auth = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails(); 
+    	String accessToken = auth.getTokenValue();
+        return studentAssessmentService.getStudentExamList(pen,accessToken);
     }
 }
