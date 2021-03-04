6931a5ad6b54a3f0de9d5d2f85d83d97c84c6235
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-03-03 17:07:16 -0800

    build fix

--- api/src/main/java/ca/bc/gov/educ/api/studentexam/config/GradStudentExamConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentexam/config/GradStudentExamConfig.java
@@ -16,6 +16,7 @@ public class GradStudentExamConfig implements WebMvcConfigurer {
 
 	@Autowired
 	RequestInterceptor requestInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(requestInterceptor);
