f1a7ae96ccd5bc4e30d5bd2bdefe784c701e376a
Kamal Mohammed <kamal.mohammed@outlook.com>
2021-05-15 13:12:31 -0700

    initial commit

---
+++ tools/jenkins/add-pipeline-job.groovy
@@ -0,0 +1,13 @@
+import hudson.plugins.git.*;
+import com.cloudbees.hudson.plugins.folder.Folder
+
+def scm = new GitSCM("https://github.com/bcgov/EDUC-STUDENT-COURSE_API.git")
+scm.branches = [new BranchSpec("main")];
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "/path/to/Jenkinsfile")
+
+def parent = Jenkins.instance.getItem('TEST-FOLDER');
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "TEST-API-PIPELINE")
+job.definition = flowDefinition
+
+parent.doReload()
