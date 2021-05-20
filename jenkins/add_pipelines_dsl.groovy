1a6d3b0bdcdaed1ab7c50485107661107defc365
Kamal <kamal.mohammed@outlook.com>
2021-05-20 06:48:11 -0700

    Updated script

--- jenkins/add_pipelines_dsl.groovy
+++ jenkins/add_pipelines_dsl.groovy
@@ -3,16 +3,48 @@ import groovy.yaml.YamlSlurper
 
 println 'Starting SCRIPT'
 
-folder('TESTING') {
-    displayName('TESTING')
-    description('Folder for Testing scripts')
+def newFolder = { folderName ->
+    folder(folderName) {
+        displayName(folderName)
+        //description('Folder for Testing scripts')
+    }
 }
 
-pipelineJob('TESTING/example') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('jenkins/Jenkinsfile'))
-            sandbox()
+def newPipeline = { String folderName, String appName, String scmUrl, String scmBranch ->
+    pipelineJob("${folderName}/${appName}") {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote { url(scmUrl) }
+                        branches(scmBranch)
+                        extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+                    }
+                }
+                scriptPath('jenkins/Jenkinsfile')
+            }
         }
     }
+}
+
+def thisBuild = Thread.currentThread().executable
+def resolver = thisBuild.buildVariableResolver
+def envName = resolver.resolve('ENV_NAME')
+
+newFolder(envName)
+
+def yamlSlurper = new YamlSlurper()
+//inventory = yamlSlurper.parseText(new URL("https://..../inventory.yaml").getText())
+inventory = yamlSlurper.parse(new File("${WORKSPACE}/jenkins/inventory.yaml"))
+apps = inventory.applications
+
+apps.each { app ->
+    println '---------------------------------------------------------------'
+    app.environments.each {
+        if (envName.equalsIgnoreCase(it.acronym)) {
+            newPipeline(it.acronym, app.name, app.git, 'main')
+            println "Pipeline Added: ${app.name}"
+        }
+    }
+    println '---------------------------------------------------------------'
 }
--- jenkins/deploy-api.groovy
+++ jenkins/deploy-api.groovy
@@ -29,6 +29,12 @@ pipeline{
     }
 
     environment{
+        OCP_PROJECT = '77c02f-dev'
+        IMAGE_PROJECT = '77c02f-tools'
+        IMAGE_TAG = 'latest'
+        APP_SUBDOMAIN_SUFFIX = '77c02f-test'
+        REPO_NAME = 'educ-grad-code-api'
+        JOB_NAME = 'main'
         APP_NAME = 'educ-grad-code-api'
         APP_DOMAIN = 'apps.silver.devops.gov.bc.ca'
     }
@@ -38,17 +44,17 @@ pipeline{
             steps{
                 script {
                     openshift.withCluster() {
-                        openshift.withProject('77c02f-dev') {
+                        openshift.withProject(OCP_PROJECT) {
                             def dcTemplate = openshift.process('-f', 'openshift/api.dc.yaml',
-                                    "REPO_NAME=educ-grad-code-api",
-                                    "JOB_NAME=main",
-                                    "NAMESPACE=${NAMESPACE}",
-                                    "APP_NAME=educ-grad-code-api",
-                                    "HOST_ROUTE=educ-grad-code-api-77c02f-test.apps.silver.devops.gov.bc.ca",
-                                    "TAG=${TAG}"
+                                    "REPO_NAME=${REPO_NAME}",
+                                    "JOB_NAME=${JOB_NAME}",
+                                    "NAMESPACE=${IMAGE_PROJECT}",
+                                    "APP_NAME=${APP_NAME}",
+                                    "HOST_ROUTE=${REPO_NAME}-${APP_SUBDOMAIN_SUFFIX}.${APP_DOMAIN}",
+                                    "TAG=${IMAGE_TAG}"
                             )
 
-                            echo "Applying Deployment educ-grad-code-api"
+                            echo "Applying Deployment ${APP_NAME}"
                             def dc = openshift.apply(dcTemplate).narrow('dc')
 
                             echo "Waiting for deployment to roll out"
