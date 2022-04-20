c1f8930607825817e6b121e9a62478c05f30f7e7
Kamal <kamal.mohammed@outlook.com>
2022-04-20 12:24:45 -0600

    Adding Keycloak files

--- Keycloak/create_roles.sh
+++ Keycloak/create_roles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 while read line
 do
-echo $line
+  curl --location --request POST 'https://soam-test.apps.silver.devops.gov.bc.ca/auth/admin/realms/master/roles' \
+  --header "Authorization: Bearer $BEARER" \
+  --header "Content-Type: application/json" \
+  --data-raw $line
 done < grad-roles.dat
