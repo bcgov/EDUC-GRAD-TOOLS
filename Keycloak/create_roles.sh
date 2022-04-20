ea222928e1845ed126e628da3e2f86237dbf1fb6
Kamal <kamal.mohammed@outlook.com>
2022-04-20 13:37:38 -0600

    Adding Keycloak files

--- Keycloak/create_roles.sh
+++ Keycloak/create_roles.sh
@@ -2,8 +2,9 @@
 # Usage: ./create_roles.sh $URL $BEARER
 while read line
 do
-  curl --write-out '%header{Host} %{response_code}, %{errormsg}' --location --request POST "$1" \
+  curl --write-out '%{header_json} %{response_code}, %{errormsg}' --location --request POST "$1" \
   --header "Authorization: Bearer $2" \
   --header "Content-Type: application/json" \
   --data-raw "$line"
+  echo -e "\n"
 done < grad-roles.dat
