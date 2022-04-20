e75553abd1a2137efcc962143b43f36e21bd0943
Kamal <kamal.mohammed@outlook.com>
2022-04-20 13:00:12 -0600

    Adding Keycloak files

--- Keycloak/create_roles.sh
+++ Keycloak/create_roles.sh
@@ -5,7 +5,7 @@ do
   RESULT=$(curl -v --location --request POST "$1" \
   --header "Authorization: Bearer $2" \
   --header "Content-Type: application/json" \
-  --data-raw "$line")
+  --data-raw "$line" 2>/dev/null)
   echo $RESULT | grep -E 'POST\|Host\|201 Created\|401 Unauthorized\|location\|errorMessage'
   echo "\n"
 done < grad-roles.dat
