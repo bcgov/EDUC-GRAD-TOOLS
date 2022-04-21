c6ce06878275d5a769409b62d92ae96848174f30
Kamal <kamal.mohammed@outlook.com>
2022-04-21 15:49:58 -0600

    Adding Keycloak files

--- Keycloak/grad_rbac_setup.sh
+++ Keycloak/grad_rbac_setup.sh
@@ -4,6 +4,7 @@
 echo -e "Keycloak URL: $1\n"
 CREATE_ROLE='/auth/admin/realms/master/roles'
 CREATE_CLIENT_SCOPE='/auth/admin/realms/master/client-scopes'
+ASSIGN_ROLES_TO_CLIENT_SCOPE='/auth/admin/realms/master/client-scopes/{client-scope}/scope-mappings/realm'
 
 #Create Roles
 echo -e "CREATE Roles\n============\nURL: $1$CREATE_ROLE\n"
@@ -30,5 +31,28 @@ do
   --header "Authorization: Bearer $2" \
   --header "Content-Type: application/json" \
   --data-raw "{\"id\": \"$CLIENT_SCOPE_TRIMMED\", \"name\": \"$CLIENT_SCOPE\", \"protocol\": \"openid-connect\", \"attributes\": { \"include.in.token.scope\": \"true\", \"display.on.consent.screen\": \"false\"}}"
+  echo -e "\n"
+done < grad-client-scopes.lst
+
+#Add
+# cat temp.json | jq .[] | jq 'select(.name == "GRAD_SYSTEM_COORDINATOR")' | jq -r .name
+echo -e "Assign Roles to Client Scopes\n============\nURL: $1$ASSIGN_ROLES_TO_CLIENT_SCOPE\n"
+
+# ADD GRAD_SYSTEM_COORDINATOR Role
+ROLE="GRAD_SYSTEM_COORDINATOR"
+echo "$1$ASSIGN_ROLES_TO_CLIENT_SCOPE" | sed -e "s/{client-scope}/${ROLE}/g"
+
+while read CLIENT_SCOPE
+do
+  #Trim scope if it's more than 38 chars long
+  CLIENT_SCOPE_TRIMMED=$CLIENT_SCOPE
+  if [ ${#str} -ge 38 ] then
+    CLIENT_SCOPE_TRIMMED=${CLIENT_SCOPE:0:37}
+  fi
+
+  curl --write-out 'URL: %{url_effective}, Response: %{response_code}' --location --request POST "$1$ASSIGN_ROLES_TO_CLIENT_SCOPE" \
+  --header "Authorization: Bearer $2" \
+  --header "Content-Type: application/json" \
+  --data-raw "[{\"id\": \"$CLIENT_SCOPE_TRIMMED\"}]"
   echo -e "\n"
 done < grad-client-scopes.lst
