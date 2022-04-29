9ee73e399117c2330af10f427b84e5511e52c4cb
Kamal <kamal.mohammed@outlook.com>
2022-04-29 11:38:09 -0600

    Script to tag openshift imagestreams

--- openshift/tag-images.sh
+++ openshift/tag-images.sh
@@ -1,10 +1,15 @@
-#Create Openshift imagestream tags
+# Create Openshift imagestream tags
+# Login to Openshift using the CLI login command before you run this script
+# ex: oc login --token=sha256~vNdSnB***********tWS9sQ --server=https://api.silver.devops.gov.bc.ca:6443
 # Pass the tag name as a command-line parameter
+# ex: sh tag-images.sh v1.3.0
 echo -e "TAG Images in Openshift\n"
+
+oc registry login
+oc project 77c02f-tools
+
 while read line
 do
-  oc registry login
-  oc project 77c02f-tools
   oc tag "$line":latest "$line":"$1"
   echo -e "\n"
 done < grad-apis.lst
