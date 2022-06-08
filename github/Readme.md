388b6abc95e5a57b00dc789107501c8b6b17feec
Kamal <kamal.mohammed@outlook.com>
2022-06-08 04:09:21 -0600

    Script to tag openshift imagestreams

---
+++ github/Readme.md
@@ -0,0 +1,20 @@
+# STEPS
+
+## Clone the Repository
+
+## Change directory to github
+
+## Update script permissions
+`chmod 744 grad_rbac_setup.sh`
+
+## SET Env Variables
+```
+URL=https://soam-test.apps.silver.devops.gov.bc.ca
+BEARER=$(curl -k -s -X POST -H 'Content-type: application/x-www-form-urlencoded' \
+        -d "client_id=admin-cli&grant_type=password&username=<cli-username>&password=<password>" \
+        $URL/auth/realms/master/protocol/openid-connect/token \
+        | jq -r '.access_token')
+```
+
+## RUN shell script
+`./grad_rbac_setup.sh $URL $BEARER`
---
+++ github/create_action_secrets.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Usage: ./create_action_secrets.sh
+
+#Get Github Token from Openshift secrets
+GITHUB_TOKEN=$(oc get secret gh-actions -n 77c02f-tools -o go-template --template="{{.data.GITHUB_TOKEN|base64decode}}")
+
+#Create Secrets
+echo -e "CREATE Secrets\n=============="
+while read REPO_NAME
+do
+  PUBKEY_EP="https://api.github.com/repos/bcgov/$REPO_NAME/actions/secrets/public-key"
+  RESP=$(curl -s --location --request GET "$PUBKEY_EP" \
+  --header "Authorization: Bearer $GITHUB_TOKEN" \
+  --header "Content-type: application/json")
+
+  PUBKEY=$(echo $RESP | jq -r .key)
+  KEY_ID=$(echo $RESP | jq -r .key_id)
+
+  echo -e "Creating Github Secrets for $REPO_NAME"
+
+  while read SECRET_KEY
+  do
+    echo $SECRET_KEY
+    SECRET_VALUE=$(oc get secret gh-actions -n 77c02f-tools -o go-template --template="{{.data.$SECRET_KEY|base64decode}}")
+    echo $SECRET_VALUE
+
+    ENCRYPTED_VALUE=$(python encryptsecret.py $PUBKEY $SECRET_VALUE)
+
+    echo -e "Encrypted Value:"$ENCRYPTED_VALUE
+
+    CREATE_SECRET_EP="https://api.github.com/repos/bcgov/$REPO_NAME/actions/secrets/$SECRET_KEY"
+
+    echo '{"key_id": "'$KEY_ID'","encrypted_value":"'$ENCRYPTED_VALUE'"}'
+
+    curl -s --location --request PUT "$CREATE_SECRET_EP" \
+      --header "Authorization: Bearer $GITHUB_TOKEN" \
+      --header "Content-Type: application/json" \
+      -d '{"key_id": "'$KEY_ID'","encrypted_value":"'$ENCRYPTED_VALUE'"}'
+
+      echo -e "\n"
+  done < secrets.lst
+
+  echo -e "\n"
+done < repos.lst
