430ecaca83e1a5eef30b918328bd250a75482084
Kamal <kamal.mohammed@outlook.com>
2022-06-29 09:15:47 -0600

    updates

--- github/create_action_secrets.sh
+++ github/create_action_secrets.sh
@@ -17,25 +17,27 @@ do
   KEY_ID=$(echo $RESP | jq -r .key_id)
 
   echo -e "Creating Github Secrets for $REPO_NAME"
-
+  #Loop through the repository secrets list
   while read SECRET_KEY
   do
     echo $SECRET_KEY
+    #Get secret value from Openshift
     SECRET_VALUE=$(oc get secret gh-actions -n 77c02f-tools -o go-template --template="{{.data.$SECRET_KEY|base64decode}}")
-    #echo $SECRET_VALUE
-
+    #Encrypt secret value using libsodium to save as a Github secret
     ENCRYPTED_VALUE=$(python3 encryptsecret.py $PUBKEY $SECRET_VALUE)
-
-    #echo -e "Encrypted Value:"$ENCRYPTED_VALUE
-
+    # PUT Endpoint to create/update a Github secret
     CREATE_SECRET_EP="https://api.github.com/repos/bcgov/$REPO_NAME/actions/secrets/$SECRET_KEY"
-
-    #echo "{\"key_id\": \"$KEY_ID\",\"encrypted_value\":\"$ENCRYPTED_VALUE\"}"
     curl -s --location --request PUT "$CREATE_SECRET_EP" \
       --header "Authorization: Bearer $GITHUB_TOKEN" \
       --header "Content-Type: application/json" \
       --data-raw "{\"key_id\": \"$KEY_ID\",\"encrypted_value\":\"$ENCRYPTED_VALUE\"}"
   done < secrets.lst
+
+  # Loop through the envs list
+  while read $ENV
+  do
+    echo $ENV
+  done < envs.lst
 
   echo -e "\n"
 done < repos.lst
