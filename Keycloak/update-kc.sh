4c883a0e71eed1d03e99ea8fdb7db5bbcd460caf
Khaled <45347802+JavaDeveloper456788@users.noreply.github.com>
2024-12-06 01:37:07 -0800

    Update update-kc.sh

--- Keycloak/update-kc.sh
+++ Keycloak/update-kc.sh
@@ -22,9 +22,9 @@ echo -e "CREATE Roles \n"
 echo "$SCRIPTS_PATH"
 while read line
 do
-  #curl -sX POST "https://$KC_BASE_URL/$KC_REALM_ID/roles" \
-  #--header "Authorization: Bearer $TKN" \
-  #--header "Content-Type: application/json" \
-  #--data-raw "$line"
+  curl -sX POST "https://$KC_BASE_URL/$KC_REALM_ID/roles" \
+  --header "Authorization: Bearer $TKN" \
+  --header "Content-Type: application/json" \
+  --data-raw "$line"
   echo -e " $line\n"
 done < roles.sh 
