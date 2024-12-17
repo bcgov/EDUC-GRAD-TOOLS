777e86c0718455a4c2f7fcdc16a4117ae36b2902
Khaled <45347802+JavaDeveloper456788@users.noreply.github.com>
2024-12-17 11:01:48 -0800

    Update update-kc.sh

--- Keycloak/update-kc.sh
+++ Keycloak/update-kc.sh
@@ -8,14 +8,16 @@ KC_REALM_ID=$4
 SCRIPTS_PATH=$5
 KC_TOKEN_URL=$6
 TKN_FILE="/tmp/token.txt"
+REFRESH_INTERVAL="30"
 
 
 
 curl -o roles.sh $SCRIPTS_PATH/grad-roles.dat
 curl -o client_scopes.sh $SCRIPTS_PATH/grad-client-scopes.lst
 curl -o clients.sh $SCRIPTS_PATH/clients.dat
+
 echo Fetching SOAM initial  token
-response=$(curl -s -v  -w POST \
+response=$(curl -s   -w POST \
   -d "client_id=admin-cli" \
   -d "username=$KC_USERNAME" \
   -d "password=$KC_PASSWORD" \
@@ -24,20 +26,19 @@ response=$(curl -s -v  -w POST \
   TKN=$(echo "$response"  | jq -r '.access_token')
   REFRESH_TOKEN=$(echo "$response"  | jq -r '.refresh_token')
   echo "$TKN" > "$TKN_FILE"
-  echo "initial refresh token $REFRESH_TOKEN"
-  
 
+  
+echo starting refresh token loop
 while true; do
-  response=$(curl -s -v  -w POST \
+  response=$(curl -s   -w POST \
     -d "client_id=admin-cli" \
     -d "grant_type=refresh_token" \
     -d "refresh_token=$REFRESH_TOKEN" \
     "$KC_TOKEN_URL/$KC_REALM_ID/protocol/openid-connect/token")
     TKN=$(echo "$response"  | jq -r '.access_token')
-    echo "refresh+ $response \n"
     REFRESH_TOKEN=$(echo "$response"  | jq -r '.refresh_token')
     echo "$TKN" > "$TKN_FILE"
-    sleep 30
+    sleep "$REFRESH_INTERVAL"
   done &
   REFRESH_PID=$!
   
@@ -73,9 +74,7 @@ done < client_scopes.sh
 
 
 #Create Clients
-cat "$TKN_FILE"
 echo -e "CREATE Clients \n"
-
 
 jq -c '.[]' clients.sh | while read -r client; do
   result=$(curl -s  -w "%{http_code}"   -X  POST "$KC_BASE_URL/$KC_REALM_ID/clients" \
