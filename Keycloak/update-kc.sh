753ad7d8d8c8e1246e0dadf86cb6678e0c6738b2
Khaled <45347802+JavaDeveloper456788@users.noreply.github.com>
2025-01-22 14:12:19 -0800

    Update update-kc.sh

--- Keycloak/update-kc.sh
+++ Keycloak/update-kc.sh
@@ -94,8 +94,12 @@ else
     echo "$clientId"
     if ! (echo "$existing_scopes" | grep -q "$scope"); then
     echo "found missing scope "$scope"  "
+    SCOPE_UUID=$(curl -s -X  GET "$KC_BASE_URL/$KC_REALM_ID/client-scopes" \
+      -H "Content-Type: application/json" \
+      -H "Authorization: Bearer "$(cat "$TKN_FILE")" "  \
+      | jq '.[] | select(.name=="'"$name"'")' | jq -r '.id')
       #PUT /{realm}/clients/{id}/default-client-scopes/{clientScopeId}
-    result=$(curl -s  -w "%{http_code}"   -X  PUT "$KC_BASE_URL/$KC_REALM_ID/clients/$CLIENT_UUID/default-client-scopes/$scope" \
+    result=$(curl -s  -w "%{http_code}"   -X  PUT "$KC_BASE_URL/$KC_REALM_ID/clients/$CLIENT_UUID/default-client-scopes/$SCOPE_UUID" \
      --header "Authorization: Bearer "$(cat "$TKN_FILE")" "  \
     --header "Content-Type: application/json" \
     )
